e212da1a5496f38653c4f4d6c7b2f55ce56fb219
Jahir Alberto Samperio <jahirsampe1234@gmail.com>
2024-07-05 11:03:42 -0600

    Primeros custom hooks

---
+++ useFetch.js
@@ -0,0 +1,88 @@
+import react, { useEffect, useState } from 'react'
+
+export const useFetch = (url) => {
+
+    //Constante de cache
+    //La idea es almacenar el url en un arreglo junto con la data del fetch
+    const localCache = {}
+
+  
+    const [state, setState] = useState({
+        data: null,
+        isLoading: true,
+        hasError: false,
+        errorMessage: null
+    })
+
+    useEffect(() => {
+
+        getFetch();
+        // setState(getFetch());
+
+        return () => {
+        
+        }
+    }, [url])
+
+    //funcion para que cargue mientras realiza la peticion a la api
+    const setLoadingState = () => {
+        setState({
+            data: null,
+            isLoading:true,
+            hasError: false,
+            error:null
+        })
+    }
+
+    const getFetch = async() => {
+
+        if(localCache[url]) {
+            console.log('Usando cache');
+            setState({
+                data: localCache[url],
+                isLoading:false,
+                hasError: false,
+                error: null
+            })
+            return;
+        }
+        
+        setLoadingState();
+
+        const resp = await fetch(url)
+
+
+        if (!resp.ok) {
+            setState({
+                data: null,
+                isLoading:false,
+                hasError: true,
+                error: {
+                    code: resp.status,
+                    message: resp.statusText,
+                }
+            })
+            return;
+        }
+
+    
+        const data = await resp.json();
+        setState({
+            data,
+            isLoading: false,
+            hasError: false,
+            error: null
+        })
+        // manejo de cache
+        localCache[url] = data;
+    }
+
+
+    
+
+    return {
+        data: state.data,
+        isLoading: state.isLoading,
+        hasError: state.hasError
+    }
+}
